7616f2123c9a6c19f5e3fe43701a0ff0ee783a99
BlindQ <blindqqz@mail.ru>
2021-09-26 20:10:47 +0300

    Deploy to Heroku 5

--- learning/settings.py
+++ learning/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = BASE_DIR, 'staticfiles'
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+django_heroku.settings(locals())
